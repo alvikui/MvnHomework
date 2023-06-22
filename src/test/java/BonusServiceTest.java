0329cabddbc22571f8b85cf0185f394651917899
Alena Kuimova <alvikui@yandex.ru>
2023-06-22 15:09:37 +0600

    Rename percent to Percent

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -34,6 +34,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
